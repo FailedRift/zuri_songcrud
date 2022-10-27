d0a8033dc81bd8f36b6579425e6a9b9d13b4d05f
FailedRift <ikennaaokon@gmail.com>
2022-10-27 10:34:58 +0100

    * Add (register) the app (musicapp) to INSTALLED_APPS in settings.py * Define model classes in models.py * make migrations using manage.py * migrate using manage.py
    Also:
    * create superuser and admin panel
    * set up enviroment variables to hide secret key
    	*installed django-environ
    	*edited settings.py
    	*created .env file and added the variables
    	*.env already in gitignore by default
    *updated requirements.txt to include django-environ

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    released_date = models.DateField()
+
+class Lyrics(models.Model):
+    content = models.CharField(max_length=2000)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
--- songcrud/songcrud/settings.py
+++ songcrud/songcrud/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9dx6gqk&df8kw5i^%78dt4n581+=yys7fj_yc_05z7wmpa9@1v'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'musicapp',
 ]
 
 MIDDLEWARE = [
