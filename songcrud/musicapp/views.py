28b9ab815d8034126662c7dc6cdaaef090f900c2
FailedRift <ikennaaokon@gmail.com>
2022-11-03 07:37:10 +0100

    * created new REST API branch * added rest_framework to settings.py * updated songcrud.urls to include musicapp.urls * created serializers.py in musicapp * added api overview to musicapp.views * added ApiOverview path to musicapp.urls

--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Artiste
+from .serializers import ArtisteSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def ApiOverview(request):
+    api_urls = {
+        'all_items': '/',
+        'Search by First Name': '/?first_name=first_name',
+        'Search by Last Name': '/?last_name=last_name',
+        'Search by Age': '/?age=age',
+        'Add': '/create',
+        'Update': '/update/pk',
+        'Delete': '/artiste/pk/delete'
+    }
+  
+    return Response(api_urls)
--- songcrud/songcrud/settings.py
+++ songcrud/songcrud/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'musicapp.apps.MusicappConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
