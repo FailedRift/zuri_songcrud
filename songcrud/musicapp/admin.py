195f6593584187fb70555aa12a9bcfed76065d0d
FailedRift <ikennaaokon@gmail.com>
2022-11-03 05:40:37 +0100

    *add __str__ definitions to models.py *registered models in admin.py of musciapp

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Artiste)
+admin.site.register(models.Song)
+admin.site.register(models.Lyrics)
+
--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -6,11 +6,17 @@ class Artiste(models.Model):
     last_name = models.CharField(max_length=100)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.first_name
+
 class Song(models.Model):
     artist = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     released_date = models.DateField()
 
+    def __str__(self):
+        return self.title
+        
 class Lyrics(models.Model):
     content = models.CharField(max_length=2000)
     song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
